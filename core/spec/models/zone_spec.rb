bfa0a7b4ba261edc4d341571231cfdf17f9f1847
Sean Schofield <sean@railsdog.com>
2011-12-06 15:21:31 -0500

    New Zone#default_tax method

--- core/spec/models/zone_spec.rb
+++ core/spec/models/zone_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe Spree::Zone do
 
-  #context 'factory' do
-    #let(:zone) { Spree::Zone.create :name => "FooZone" }
-    #it "should set zone members correctly" do
-      #zone.zone_members.count.should == 1
-    #end
-  #end
-
   context "#destroy" do
     let(:zone) { Spree::Zone.create :name => "FooZone" }
 
@@ -57,6 +50,34 @@ describe Spree::Zone do
     context "when there are no qualifying zones" do
       it "should return nil" do
         Spree::Zone.match(Spree::Address.new).should be_nil
+      end
+    end
+  end
+
+  context ".default_tax" do
+    context "when :default_tax_zone preference is specified" do
+      before { Spree::Config.set(:default_tax_zone => "foo") }
+
+      it "should be the correct zone if a zone exists with that name" do
+        foo_zone = Factory(:zone, :name => "foo")
+        Spree::Zone.default_tax.should == foo_zone
+      end
+
+      it "should be nil if no zone exists with that name" do
+        bar_zone = Factory(:zone, :name => "bar")
+        Spree::Zone.default_tax.should be_nil
+      end
+
+      it "should be nil if no zones exist" do
+        Spree::Zone.default_tax.should be_nil
+      end
+    end
+
+    context "when :default_tax_zone preference is nil" do
+      before { Spree::Config.set(:default_tax_zone => nil) }
+
+      it "should be_nil" do
+        Spree::Zone.default_tax.should be_nil
       end
     end
   end
